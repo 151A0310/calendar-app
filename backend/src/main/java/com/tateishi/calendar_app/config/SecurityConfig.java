92c7368dbc26cbc201b4f1c3d13ac5a1af0900b0
151A0310 <yuutate36@gmail.com>
2026-05-02 15:16:37 +0900

    #2 APIURLの修正

--- backend/src/main/java/com/tateishi/calendar_app/config/SecurityConfig.java
+++ backend/src/main/java/com/tateishi/calendar_app/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.tateishi.calendar_app;
+package com.tateishi.calendar_app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
