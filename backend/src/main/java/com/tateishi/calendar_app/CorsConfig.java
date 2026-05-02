bba44d0c13ad7339603b858260c23027efb30376
151A0310 <yuutate36@gmail.com>
2026-05-02 15:03:03 +0900

    APIURLを修正

--- backend/src/main/java/com/tateishi/calendar_app/CorsConfig.java
+++ backend/src/main/java/com/tateishi/calendar_app/CorsConfig.java
@@ -14,10 +14,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedOrigins("https://calendar-frontend-echp.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
             }
         };
     }
