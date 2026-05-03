59afbecf1d78002f19c84f99419f56abe955691d
151A0310 <yuutate36@gmail.com>
2026-05-04 05:29:30 +0900

    deploy

--- frontend/src/utils/authFetch.js
+++ frontend/src/utils/authFetch.js
@@ -1,6 +1,9 @@
 const API_URL = process.env.REACT_APP_API_URL;
 
 export default async function authFetch(path, options = {}) {
+
+    console.log("API_URL =", API_URL);
+
     const token = localStorage.getItem("token");
 
     return fetch(`${API_URL}${path}`, {
